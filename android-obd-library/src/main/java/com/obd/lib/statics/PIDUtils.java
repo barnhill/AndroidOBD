f9b00b6798b0064c5d390ae03d2d968589cb841c
unknown <Brad@Pnuema.cerner.com>
2015-01-31 22:12:52 -0600

    cache pids when requested to speed up requests, java doc, upgrade gradle plugin to 1.0.1

--- android-obd-library/src/main/java/com/obd/lib/statics/PIDUtils.java
+++ android-obd-library/src/main/java/com/obd/lib/statics/PIDUtils.java
@@ -23,8 +23,10 @@ import org.joda.time.DateTime;
 import org.joda.time.Period;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.TreeMap;
 
 /**
  * Class to hold all the static methods necessary for the OBD library.
@@ -34,36 +36,62 @@ import java.util.List;
  */
 public class PIDUtils {
     public static final String TAG = PIDUtils.class.getSimpleName();
-    public static final HashMap<Integer, HashMap<String, PID>> pidHashMap = new HashMap<>();
+    public static final HashMap<Integer, TreeMap<Integer, PID>> pidHashMap = new HashMap<>();
 
+    /**
+     * Gets list of pids for the mode specified
+     *
+     * @param context Context of the application
+     * @param mode Mode to look up the list of pids
+     * @return List of PIDS contained in the specified mode
+     * @throws IOException
+     */
     public static List<PID> getPidList(Context context, int mode) throws IOException {
-        return (List<PID>) getPidMap(context, mode).values();
+        return new ArrayList<>(getPidMap(context, mode).values());
     }
 
+    /**
+     * Gets PID object by mode and pid.
+     *
+     * @param context Context of the application
+     * @param mode Mode to look the pid up in
+     * @param pid Pid number to retrieve
+     * @return PID object
+     * @throws IOException
+     */
     public static PID getPid(Context context, int mode, String pid) throws IOException {
         DateTime start = DateTime.now();
 
-        HashMap<String, PID> pids = getPidMap(context, mode);
+        TreeMap<Integer, PID> pids = getPidMap(context, mode);
 
         if (pids == null) {
             Log.d(TAG, "Pids for this mode do not exist.");
             return null;
         }
 
-        PID p = pids.get(pid);
+        PID p = pids.get(Integer.parseInt(pid, 16));
         Log.d(TAG, "Found pid " + p.PID + " in " + new Period(start, DateTime.now()).getMillis() + " ms");
 
         return p;
     }
 
-    private static HashMap<String, PID> getPidMap(Context context, int mode) throws IOException {
+    private static TreeMap<Integer, PID> getPidMap(Context context, Integer mode) throws IOException {
         if (pidHashMap.containsKey(mode)) {
+            //get value from pid cache
             return pidHashMap.get(mode);
         } else {
+            //not found in cache so read it from json files and store it in cache
             List<PID> pidList = new Gson().fromJson(FileUtils.readFromFile(context, "pids-mode" + mode + ".json"), PIDS.class).pids;
-            HashMap<String, PID> pidMap = new HashMap<>();
+            TreeMap<Integer, PID> pidMap = new TreeMap<>();
             for (PID pid : pidList) {
-                pidMap.put(pid.PID, pid);
+                int pidInt = 0;
+                try {
+                    pidInt = Integer.parseInt(pid.PID, 16);
+                } catch (NumberFormatException nfex) {
+                    Log.d(TAG, "Parsing PID number to integer failed: " + nfex.getMessage());
+                }
+
+                pidMap.put(pidInt, pid);
             }
             pidHashMap.put(mode, pidMap);
             return pidHashMap.get(mode);
